5d8fecbc2e15b29d5c2d6f8f392b52c3dd019bca
Egor Turchinovich <jieggii.contact@gmail.com>
2020-04-20 19:16:05 +0300

    Update

--- update.sh
+++ update.sh
@@ -2,14 +2,14 @@
 rm -rf config && mkdir config && cd config/
 cp -r ~/.config/rofi .
 cp -r ~/.config/ranger .
-cp -r ~/.config/neofetch .
 cp -r ~/.config/termite .
 cp -r ~/.config/sublime-text-3 .
 cp -r ~/.config/polybar .
+cp ~/.config/compton.conf .
 cd ..
 
 rm -rf i3 && mkdir i3 && cd i3/
 cp -r ~/.i3/config .
 cd ..
 
-cp ~/.config/compton.conf .
+cp ~/.zshrc .
