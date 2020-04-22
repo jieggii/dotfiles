6ebf9804968b464b47ac7fe87fcf2fed6c73f409
Egor Turchinovich <jieggii.contact@gmail.com>
2020-04-22 14:08:22 +0300

    Refactoring

--- update.sh
+++ update.sh
@@ -1,15 +1,16 @@
 
-rm -rf config && mkdir config && cd config/
+rm -rf .config && mkdir .config && cd .config/
 cp -r ~/.config/rofi .
 cp -r ~/.config/ranger .
 cp -r ~/.config/termite .
 cp -r ~/.config/sublime-text-3 .
-cp -r ~/.config/polybar .
+#cp -r ~/.config/polybar .
 cp ~/.config/compton.conf .
 cd ..
 
-rm -rf i3 && mkdir i3 && cd i3/
+rm -rf .i3 && mkdir .i3 && cd .i3/
 cp -r ~/.i3/config .
 cd ..
 
 cp ~/.zshrc .
+cp ~/.profile .
